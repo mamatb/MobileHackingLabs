5150b0edac57e162de3a5e22d7b235fb61cb4177
mamatb <42239435+mamatb@users.noreply.github.com>
2026-02-16 17:11:26 +0100

    minor style changes

--- Strings/com.mobilehackinglab.strings.js
+++ Strings/com.mobilehackinglab.strings.js
@@ -16,19 +16,15 @@ Java.perform(function() {
     onComplete: function() {},
   });
 
-  const secretKeySpec = SecretKeySpec.$new(
-    String.$new("your_secret_key_1234567890123456").getBytes(), "AES");
-  const ivParameterSpec = IvParameterSpec.$new(
-    String.$new("1234567890123456").getBytes());
+  const keyBytes = String.$new("your_secret_key_1234567890123456").getBytes();
+  const ivBytes = String.$new("1234567890123456").getBytes();
   let cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-  cipher.init(0b10, secretKeySpec, ivParameterSpec);
-  const path = Base64.encodeToString(
-    cipher.doFinal(Base64.decode("bqGrDKdQ8zo26HflRsGvVA==", 0)), 0);
+  cipher.init(0b10, SecretKeySpec.$new(keyBytes, "AES"), IvParameterSpec.$new(ivBytes));
+  const path = Base64.encodeToString(cipher.doFinal(Base64.decode("bqGrDKdQ8zo26HflRsGvVA==", 0)), 0);
   console.log("[+] path decrypted");
 
   let intent = Intent.$new();
-  intent.setClassName("com.mobilehackinglab.challenge",
-    "com.mobilehackinglab.challenge.Activity2");
+  intent.setClassName("com.mobilehackinglab.challenge", "com.mobilehackinglab.challenge.Activity2");
   intent.setAction("android.intent.action.VIEW");
   intent.setData(Uri.parse("mhl://labs/" + path));
   intent.addFlags(0x10000000);
@@ -38,7 +34,7 @@ Java.perform(function() {
   console.log("[+] activity started");
 
   setTimeout(function() {
-    const module = Process.getModuleByName("libflag.so"), pattern = "MHL{";
+    const pattern = "MHL{", module = Process.getModuleByName("libflag.so");
     let patternHex = [];
     for (let i = 0; i < pattern.length; i++) {
       patternHex.push(pattern.charCodeAt(i).toString(0x10));
