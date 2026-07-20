69a8e54332bdabbb926d4ecd0a20fc6fe89e75f0
mamatb <amatbaeza@proton.me>
2026-07-20 10:11:34 +0200

    Food Store lab done

---
+++ Food Store/com.mobilehackinglab.foodstore.sh
@@ -0,0 +1,9 @@
+adb install com.mobilehackinglab.foodstore.apk
+adb shell 'am start -n com.mobilehackinglab.foodstore/.LoginActivity'
+
+# Sign up
+#
+# user', 'cGFzc3dvcmQ=', 'YWRkcmVzcw==', 1); #
+# password
+# address
+
